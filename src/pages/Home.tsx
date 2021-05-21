77d963c3df5a78279dce8b0f21576e16c74d95fe
Kalil Peixoto <kalil.peixoto@jupiter.co.ao>
2021-05-21 11:32:23 +0100

    First react native challenge 01

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,32 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+      if (newTaskTitle) {
+
+        const task: Task = {
+            id: new Date().getTime(),
+            title: newTaskTitle,
+            done: false
+        }
+
+        setTasks(oldState => [...oldState, task]);
+      }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+      // make a copy so you dont change the state directly
+      const tasksCopy = tasks.slice();
+      let taskToBeEdited: Task = tasksCopy.find(task => task.id === id)!;
+      taskToBeEdited.done = !taskToBeEdited.done;
+
+      setTasks(tasksCopy);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
@@ -31,10 +45,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
   )
